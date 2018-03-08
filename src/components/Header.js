87c1d3261ebf919e25cc4c29d25db9148d341bc3
PERRUCCI ISRAEL <B040825@rio.ar.bsch>
2018-03-08 16:52:54 -0300

    links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ const Header = () => {
 	return (
 		<div className="component-header">
 			<h1 className="App-title">React Cards</h1>
-			<Link to="./">Home</Link>
-			<Link to="{`${process.env.PUBLIC_URL}/config`}">Config</Link>
-			<Link to="./game">Game</Link>
+			<Link to="/">Home</Link>
+			<Link to="/config">Config</Link>
+			<Link to="/game">Game</Link>
 		</div>
 	);
 };
