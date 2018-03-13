4c78c1e26e4e3ba9c69885c1d673c252c5ad8fb0
GerardoMac <centrodph@gmail.com>
2018-03-13 07:21:20 -0300

    flickr

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ You can find the most recent version of this guide [here](https://github.com/fac
 https://gist.github.com/cobyism/4730490
 
 https://centrodph.github.io/react-cards/
+
+https://api.instagram.com/oauth/authorize/?client_id=ee22633209d7479aacda1f7a2394d7d6&redirect_uri=http://localhost:3000/&response_type=code&scope=public_content
+
+http://localhost:3000/?code=64e3398d3cce4963adf319ad74bcc8e0
+
+https://www.flickr.com/services/apps/72157694438378195/key/
+dc3e0ee29c9c2153cdd80f478d643fb8
+
+https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=dc3e0ee29c9c2153cdd80f478d643fb8&tags=apple&per_page=3&format=json
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,121 +1,150 @@
 import React, { Component } from 'react';
 import Card from '../components/Card';
+import axios from 'axios';
 
 class Game extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		const cards = this.shuffle([...this.props.cards, ...this.props.cards]); //duplicate
+        const cards = this.shuffle([...this.props.cards, ...this.props.cards]); //duplicate
 
-		this.state = {
-			waiting: false,
-			qtySelected: 0,
-			indexSelected: [],
-			finded: [],
-			cards
-		};
-	}
-	shuffle(items) {
-		return items
-			.map(item => {
-				return {
-					weight: Math.random(),
-					value: item
-				};
-			})
-			.sort((a, b) => {
-				return a.weight - b.weight;
-			})
-			.map(item => {
-				return item.value;
-			});
-	}
+        this.state = {
+            images: [],
+            waiting: false,
+            qtySelected: 0,
+            indexSelected: [],
+            finded: [],
+            cards
+        };
+    }
 
-	clean() {
-		setTimeout(() => {
-			this.setState({
-				waiting: false,
-				qtySelected: 0,
-				indexSelected: []
-			});
-		}, this.props.delay);
-	}
+    componentDidMount = async () => {
+        const data = {
+            method: 'flickr.photos.search',
+            api_key: 'dc3e0ee29c9c2153cdd80f478d643fb8',
+            tags: 'dog,apple,carots',
+            per_page: 25,
+            format: 'json'
+        };
+        const query = Object.keys(data)
+            .map(function(key) {
+                return encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
+            })
+            .join('&');
 
-	handleCardClick = index => {
-		if (this.state.waiting === true) return;
+        const result = await axios.get('https://api.flickr.com/services/rest/?' + query);
+        this.setState({ images: result.photo });
+    };
 
-		if (this.state.indexSelected.indexOf(index) === -1) {
-			const indexSelected = [...this.state.indexSelected, index];
-			const qtySelected = indexSelected.length;
-			const waiting = qtySelected === 2;
-			let finded = [];
-			if (waiting && this.state.cards[indexSelected[0]].id === this.state.cards[indexSelected[1]].id) {
-				finded = [...indexSelected];
-			}
+    shuffle(items) {
+        return items
+            .map((item) => {
+                return {
+                    weight: Math.random(),
+                    value: item
+                };
+            })
+            .sort((a, b) => {
+                return a.weight - b.weight;
+            })
+            .map((item) => {
+                return item.value;
+            });
+    }
 
-			this.setState({
-				waiting,
-				qtySelected,
-				indexSelected,
-				finded: [...this.state.finded, ...finded]
-			});
-		}
-	};
+    clean() {
+        setTimeout(() => {
+            this.setState({
+                waiting: false,
+                qtySelected: 0,
+                indexSelected: []
+            });
+        }, this.props.delay);
+    }
 
-	renderCards() {
-		return this.state.cards.map((card, index) => {
-			const flipped = this.state.indexSelected.indexOf(index) > -1;
-			const finded = this.state.finded.indexOf(index) > -1;
-			return <Card key={index} flipped={flipped} finded={finded} {...card} onClick={() => this.handleCardClick(index)} />;
-		});
-	}
+    handleCardClick = (index) => {
+        if (this.state.waiting === true) return;
 
-	renderDebug() {
-		return this.state.indexSelected.join(' - ');
-	}
+        if (this.state.indexSelected.indexOf(index) === -1) {
+            const indexSelected = [...this.state.indexSelected, index];
+            const qtySelected = indexSelected.length;
+            const waiting = qtySelected === 2;
+            let finded = [];
+            if (waiting && this.state.cards[indexSelected[0]].id === this.state.cards[indexSelected[1]].id) {
+                finded = [...indexSelected];
+            }
 
-	renderFinded() {
-		return this.state.finded.length;
-	}
+            this.setState({
+                waiting,
+                qtySelected,
+                indexSelected,
+                finded: [...this.state.finded, ...finded]
+            });
+        }
+    };
 
-	render() {
-		if (this.state.waiting === true) {
-			this.clean();
-		}
-		if (this.state.finded.length === this.state.cards.length) {
-			alert('GANASTE !!!!!');
-		}
+    renderCards() {
+        return this.state.cards.map((card, index) => {
+            const flipped = this.state.indexSelected.indexOf(index) > -1;
+            const finded = this.state.finded.indexOf(index) > -1;
+            return (
+                <Card
+                    key={index}
+                    flipped={flipped}
+                    finded={finded}
+                    {...card}
+                    onClick={() => this.handleCardClick(index)}
+                />
+            );
+        });
+    }
 
-		return (
-			<div className="component-game">
-				<div className="component-game-structure">
-					<div className="component-game-box">{this.renderCards()}</div>
-					<div className="component-game-score">
-						<ul>
-							<li>Selected: {this.renderDebug()}</li>
-							<li>finded: {this.renderFinded()}</li>
-							<li>QTY: {this.state.qtySelected}</li>
-							<li>
-								Waiting: {this.state.waiting && ` true`}
-								{!this.state.waiting && ` false`}
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		);
-	}
+    renderDebug() {
+        return this.state.indexSelected.join(' - ');
+    }
+
+    renderFinded() {
+        return this.state.finded.length;
+    }
+
+    render() {
+        if (this.state.waiting === true) {
+            this.clean();
+        }
+        if (this.state.finded.length === this.state.cards.length) {
+            alert('GANASTE !!!!!');
+        }
+
+        return (
+            <div className="component-game">
+                <div className="component-game-structure">
+                    <div className="component-game-box">{this.renderCards()}</div>
+                    <div className="component-game-score">
+                        <ul>
+                            <li>Selected: {this.renderDebug()}</li>
+                            <li>finded: {this.renderFinded()}</li>
+                            <li>QTY: {this.state.qtySelected}</li>
+                            <li>
+                                Waiting: {this.state.waiting && ` true`}
+                                {!this.state.waiting && ` false`}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 Game.defaultProps = {
-	delay: 1500,
-	cards: [
-		{ background: 'red', title: 'Puka', id: 1 },
-		{ background: 'green', title: 'Mora', id: 2 },
-		{ background: 'yellow', title: 'Rapunzel', id: 3 },
-		{ background: 'violet', title: 'Elsa', id: 4 },
-		{ background: 'blue', title: 'Ana', id: 5 }
-	]
+    delay: 1500,
+    cards: [
+        { background: 'red', title: 'Puka', id: 1 },
+        { background: 'green', title: 'Mora', id: 2 },
+        { background: 'yellow', title: 'Rapunzel', id: 3 },
+        { background: 'violet', title: 'Elsa', id: 4 },
+        { background: 'blue', title: 'Ana', id: 5 }
+    ]
 };
 
 export default Game;
